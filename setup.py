0e9ffeca417f9c4b07187f4ff073599d3ded2f10
Julian van Doorn <jvdoorn@antarc.com>
2020-11-12 12:22:15 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="spectral",
-    version="0.1.4",
+    version="0.1.5",
     author="Julian van Doorn",
     author_email="jvdoorn@antarc.com",
     url="https://github.com/jvdoorn/SpectrumAnalyzer",
