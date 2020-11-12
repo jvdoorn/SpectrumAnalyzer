9f9d5aa8d632090d1b13f7cd955b8b466fd1de9f
Julian van Doorn <jvdoorn@antarc.com>
2020-11-12 11:00:44 +0100

    Minor improvements in setup.py.

--- setup.py
+++ setup.py
@@ -2,9 +2,19 @@ import setuptools
 
 setuptools.setup(
     name="spectral",
-    version="0.1.0",
+    version="0.1.1",
     author="Julian van Doorn",
     author_email="jvdoorn@antarc.com",
     url="https://github.com/jvdoorn/SpectrumAnalyzer",
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    project_urls={
+        "Source": "https://github.com/jvdoorn/SpectrumAnalyzer",
+        "Tracker": "https://github.com/jvdoorn/SpectrumAnalyzer/issues",
+    },
 )
