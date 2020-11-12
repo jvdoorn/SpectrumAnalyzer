8d3a5495acf2bd4b4c23290e654fedbb65dc34b5
Julian van Doorn <jvdoorn@antarc.com>
2020-11-12 10:38:58 +0100

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="spectral",
+    version="0.1.0",
+    author="Julian van Doorn",
+    author_email="jvdoorn@antarc.com",
+    url="https://github.com/jvdoorn/SpectrumAnalyzer",
+    packages=setuptools.find_packages()
+)
