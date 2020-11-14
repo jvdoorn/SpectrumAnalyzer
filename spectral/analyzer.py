Fixed calculation of intensity.

spectral/analyzer.py
@@ -332,8 +332,9 @@ class SimulationAnalyzer(Analyzer):
             output_frequencies, output_fft = input_frequencies, transfer_function(frequency) * input_fft
 
             # Determine the output power compared to the input power
-            intensity = power(output_frequencies, output_fft, frequency, self._df) / power(input_frequencies, input_fft,
-                                                                                           frequency, self._df)
+            intensity = np.sqrt(
+                power(output_frequencies, output_fft, frequency, self._df) / power(input_frequencies, input_fft,
+                                                                                   frequency, self._df))
 
             # Determine the phases of the input and output signal
             input_phase = np.angle(input_fft)[find_nearest_index(input_frequencies, frequency)]