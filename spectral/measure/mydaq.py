dd8662b887b8940f5b3de9d597f24580de3102fd
Julian van Doorn <jvdoorn@antarc.com>
2021-01-15 12:35:11 +0100

    Refactored the MyDAQ class and set the maximum sample rate to 2e5

--- spectral/measure/mydaq.py
+++ spectral/measure/mydaq.py
@@ -14,40 +14,47 @@ from spectral.measure.measurer import Measurer
 
 
 class MyDAQ(Measurer):
+    MAXIMUM_SAMPLE_RATE = int(2e5)
+
+    @staticmethod
+    def register_output_channels(task: dx.Task, channels: np.ndarray):
+        for channel in channels:
+            task.ao_channels.add_ao_voltage_chan(channel)
+
+    @staticmethod
+    def register_input_channels(task: dx.Task, channels: np.ndarray):
+        for channel in channels:
+            task.ai_channels.add_ai_voltage_chan(channel)
+
+    def configure_timings(self, task: dx.Task, samples: int):
+        task.timing.cfg_samp_clk_timing(self.sample_rate, sample_mode=dx.constants.AcquisitionType.FINITE,
+                                        samps_per_chan=np.long(samples))
+
     def write(self, voltages: np.ndarray, channels: np.ndarray, samples: int):
         with dx.Task() as task:
-            for channel in channels:
-                task.ao_channels.add_ao_voltage_chan(channel)
+            self.register_output_channels(task, channels)
+            self.configure_timings(task, samples)
 
-            task.timing.cfg_samp_clk_timing(self.sample_rate, sample_mode=dx.constants.AcquisitionType.FINITE,
-                                            samps_per_chan=np.long(samples))
             task.write(voltages, auto_start=True)
             time.sleep(np.long(samples) / self.sample_rate + 0.0001)
             task.stop()
 
     def read(self, channels: np.ndarray, samples: int) -> Tuple[np.ndarray, np.ndarray]:
         with dx.Task() as task:
-            for channel in channels:
-                task.ai_channels.add_ai_voltage_chan(channel)
+            self.register_input_channels(task, channels)
+            self.configure_timings(task, samples)
 
-            task.timing.cfg_samp_clk_timing(self.sample_rate, sample_mode=dx.constants.AcquisitionType.FINITE,
-                                            samps_per_chan=np.long(samples))
-            return task.read(number_of_samples_per_channel=samples,
-                             timeout=dx.constants.WAIT_INFINITELY), self.calculate_time_array(samples)
+            data = task.read(number_of_samples_per_channel=samples, timeout=dx.constants.WAIT_INFINITELY)
+            return data, self.calculate_time_array(samples)
 
     def read_write(self, voltages: np.ndarray, write_channels: np.ndarray, read_channels: np.ndarray, samples: int) -> \
             Tuple[np.ndarray, np.ndarray]:
         with dx.Task() as write_task, dx.Task() as read_task:
-            for channel in write_channels:
-                write_task.ao_channels.add_ao_voltage_chan(channel)
+            self.register_output_channels(write_task, write_channels)
+            self.register_input_channels(read_task, read_channels)
 
-            for channel in read_channels:
-                read_task.ai_channels.add_ai_voltage_chan(channel)
-
-            read_task.timing.cfg_samp_clk_timing(self.sample_rate, sample_mode=dx.constants.AcquisitionType.FINITE,
-                                                 samps_per_chan=np.long(samples))
-            write_task.timing.cfg_samp_clk_timing(self.sample_rate, sample_mode=dx.constants.AcquisitionType.FINITE,
-                                                  samps_per_chan=np.long(samples))
+            self.configure_timings(write_task, samples)
+            self.configure_timings(read_task, samples)
 
             write_task.write(voltages, auto_start=True)
             data = read_task.read(number_of_samples_per_channel=samples, timeout=dx.constants.WAIT_INFINITELY)
