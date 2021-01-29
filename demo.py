e4e09bb067ba8602b332bf8c1dcb87e0caaf911d
Julian van Doorn <jvdoorn@antarc.com>
2021-01-29 16:49:07 +0100

    Cleaned up demo.

--- demo.py
+++ demo.py
@@ -20,10 +20,8 @@ frequencies = np.logspace(0, 4, 8 * 12)
 
 if __name__ == '__main__':
     analyzer = SimulationAnalyzer()
-    # analyzer.plot(f"Simulation of low pass filter with $RC={RC_neat}$.",
-    #               *analyzer.simulate_transfer_function(frequencies, low_pass), plot_gradient=True)
-    analyzer.plot(f"Prediction of low pass filter with $RC={RC_neat}$.", analyzer.predict(frequencies, low_pass))
-    # analyzer.plot(f"Simulation of high pass filter with $RC={RC_neat}$.",
-    #               *analyzer.simulate_transfer_function(frequencies, low_pass), plot_gradient=True)
-    analyzer.plot(f"Prediction of high pass filter with $RC={RC_neat}$.", analyzer.predict(frequencies, high_pass),
-                  phase_markers=[np.pi / 4])
+    plot = analyzer.plot(f"Prediction of low pass filter with $RC={RC_neat}$.", analyzer.predict(frequencies, low_pass))
+    plot.show()
+    plot = analyzer.plot(f"Prediction of high pass filter with $RC={RC_neat}$.",
+                         analyzer.predict(frequencies, high_pass))
+    plot.show()
